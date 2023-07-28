d481676e4112fd3659321c092dd8e8b5b6230ec0
JacobR1115 <123122021+JacobR1115@users.noreply.github.com>
2023-07-27 19:12:21 -0500

    Add files via upload

---
+++ part02-Part02_03.AbsoluteValue/src/main/java/AbsoluteValue.java
@@ -0,0 +1,18 @@
+
+import java.util.Scanner;
+
+public class AbsoluteValue {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int x = Integer.valueOf(scanner.nextLine());
+        if (x < 0) {
+            x = x * -1;
+            System.out.println(x);
+        } else {
+            System.out.println(x);
+        }
+
+    }
+}
